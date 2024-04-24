eee7b0132786ee9d7254741659488dad37f192a0
dzemor88 <165244834+dzemor88@users.noreply.github.com>
2024-04-24 13:55:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -13,34 +13,25 @@ const clouds = document.querySelector('.clouds');
 const visibilty = document.querySelector('.visibilty');
 const aqiDisplay = document.querySelector('.aqi');
 
+const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=65e525b678aadd227ebca31192f8550e';
+const apiUnits = '&units=metric';
+const apiLang = '&lang=pl';
 
 
 function getWeather() {
-    const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
-    const apiKey = '&appid=65e525b678aadd227ebca31192f8550e';
-    const apiUnits = '&units=metric';
-    const apiLang = '&lang=pl';
     const apiCity = input.value;
-
     const URL = apiLink + apiCity + apiKey + apiUnits + apiLang;
     console.log(URL);
 
     axios.get(URL)
         .then(response => {
-            aqi = getAirQuality(apiCity);
             console.log(response);
-            img.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-            city.textContent = `Miasto: ${response.data.name} Kraj: ${response.data.sys.country}`;
-            temp.textContent = `${response.data.main.temp} stopni Celsjusza`;
-            description.textContent = `Opis: ${response.data.weather[0].description}`;
-            feelLike.textContent = `${response.data.main.feels_like} stopni Celsjusza`;
-            humidity.textContent = `${response.data.main.humidity} %`;
-            pressure.textContent = `${response.data.main.pressure} hPa`;
-            windSpeed.textContent = `${response.data.wind.speed} m/s`;
-            clouds.textContent = `${response.data.clouds.all} %`;
-            visibilty.textContent = `${response.data.visibility / 1000} km`;
-            aqi.textContent = ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor'][aqi - 1]
-            errorMessage.textContent = '';
+            updateWeatherInfo(response.data);
+            return getAirQuality(response.data.coord.lat, response.data.coord.lon);
+        })
+        .then(aqiData => {
+            updateAQIDisplay(aqiData);
         })
         .catch(error => {
             errorMessage.textContent = `${error.response.data.message}`;
@@ -54,21 +45,35 @@ function getWeather() {
         })
 }
 
+function updateWeatherInfo(data) {
+    console.log(data);
+    img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    city.textContent = `Miasto: ${data.name} Kraj: ${data.sys.country}`;
+    temp.textContent = `${data.main.temp} stopni Celsjusza`;
+    description.textContent = `Opis: ${data.weather[0].description}`;
+    feelLike.textContent = `${data.main.feels_like} stopni Celsjusza`;
+    humidity.textContent = `${data.main.humidity} %`;
+    pressure.textContent = `${data.main.pressure} hPa`;
+    windSpeed.textContent = `${data.wind.speed} m/s`;
+    clouds.textContent = `${data.clouds.all} %`;
+    visibility.textContent = `${data.visibility / 1000} km`;
+}
+
+function getAirQuality(lat, lon) {
+    const airApiUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}${apiKey}`;
+    return axios.get(airApiUrl).then(res => res.data);
+}
+
+function updateAQIDisplay(aqiData) {
+    const aqi = aqiData.list[0].main.aqi;
+    aqiDisplay.textContent = `Jakość powietrza (AQI): ${['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor'][aqi - 1]}`;
+}
+
 const getWeatherByEnter = (e) => {
     if (e.key === 'Enter') {
         getWeather();
     }
 }
-
-function getAirQuality(apiCity) {
-    const airApiLink = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=';
-    const airApiKey = '&appid=65e525b678aadd227ebca31192f8550e';
-    const airURL = `${airApiLink}${apiCity}${airApiKey}`;
-
-    return axios.get(airURL).then(response => response.datalist[0].main.aqi);
-}
-
-
 
 button.addEventListener('click', getWeather)
 
